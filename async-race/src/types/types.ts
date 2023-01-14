3d996daf329f972cea3443f72ee7e684a4d16e5a
Andrey <spacecmetanka@gmail.com>
2023-01-15 01:27:43 +0300

    feat: add engine tepes

--- async-race/src/types/types.ts
+++ async-race/src/types/types.ts
@@ -4,9 +4,19 @@ export interface ICar {
   color: string;
 }
 
+export interface IEngine {
+  velocity: number;
+  distance: number;
+}
+
 export enum METHODS {
   POST = 'POST',
   PUT = 'PUT',
   DELETE = 'DELETE',
   PATCH = 'PATCH'
+}
+
+export enum ENGINESTATE {
+  STARTED = 'started',
+  STOPPED = 'stopped'
 }
