7e0c814cc01985f6a0821677e2baa28dd777e6dc
Andrey <spacecmetanka@gmail.com>
2023-01-17 20:27:14 +0300

    feat: implement router

--- async-race/src/components/App.ts
+++ async-race/src/components/App.ts
@@ -1,39 +1,22 @@
-import MainPage from './MainPage';
 import Navigation from './Navigation';
-import Winners from './Winners';
+import Router from './Router';
 
 export default class App {
-  private container: HTMLElement;
-  private MainPage: MainPage;
-  private winners: Winners;
-  private navigation: Navigation;
-  constructor() {
-    this.container = document.body;
-    this.navigation = new Navigation();
-    this.winners = new Winners();
-    this.MainPage = new MainPage();
-  }
+  private body: HTMLElement = document.body;
+  private rootElement: HTMLElement = document.createElement('div');
 
-  static changePage() {
-    window.addEventListener('hashchange', () => {
-      const hash = window.location.hash.slice(1);
-      const winnersPage = document.querySelector('.winners');
-      const garagePage = document.querySelector('.garage');
-      if (hash === 'garage') {
-        (winnersPage as HTMLElement).classList.add('hidden');
-        (garagePage as HTMLElement).classList.remove('hidden');
-      } else if (hash === 'winners') {
-        (garagePage as HTMLElement).classList.add('hidden');
-        (winnersPage as HTMLElement).classList.remove('hidden');
-      }
-    });
+  private navigation: Navigation;
+  private router: Router;
+
+  constructor() {
+    this.rootElement.classList.add('root');
+    this.navigation = new Navigation();
+    this.router = new Router(this.rootElement);
   }
 
   async start() {
-    this.container.append(this.navigation.render());
-    this.container.append(await this.MainPage.render());
-
-    this.container.append(this.winners.render());
-    App.changePage();
+    this.body.append(this.navigation.render());
+    this.body.append(this.rootElement);
+    this.router.initRouter();
   }
 }
