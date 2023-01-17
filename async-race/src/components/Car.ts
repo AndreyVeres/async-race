5195187445518b3c165f8bf39335a9e5ba01ca0b
Andrey <spacecmetanka@gmail.com>
2023-01-17 17:51:40 +0300

    refactor: car component

--- async-race/src/components/Car.ts
+++ async-race/src/components/Car.ts
@@ -1,76 +1,88 @@
 import {
-  ICar, METHODS, ENGINESTATE, IEngine
+  ICar, ENGINESTATE, IAnimations, IEngine
 } from '../types/types';
 import View from './View';
 import flagImage from '../assets/racing-flag.svg';
 import Loader from './Loader';
+import animation from './animation';
+
+interface IControlsButton {
+  button: HTMLButtonElement;
+  state: boolean;
+}
+
+interface ICarControls {
+  start: IControlsButton;
+  stop: IControlsButton;
+}
 
 export default class Car {
-  private car: ICar;
+  public car: ICar;
   private container: HTMLElement;
-  private carImage: HTMLElement | null;
   private loader: Loader;
-  private velocity: number | null;
-  private distance: number | null;
-  private engineStatus: string;
+  static animations: IAnimations = {};
+  private carControls: ICarControls;
+  private driver: HTMLElement;
+  private engineStatus = ENGINESTATE.STOPPED;
   constructor(car: ICar) {
     this.car = car;
-    this.engineStatus = ENGINESTATE.STOPPED;
-    this.velocity = null;
-    this.distance = null;
     this.loader = new Loader();
     this.container = this.getCarHTML();
-    this.carImage = this.container.querySelector('.car');
+    this.driver = this.container.querySelector('.car') as HTMLElement;
+    this.carControls = {
+      start: {
+        button: this.container.querySelector('.start-button') as HTMLButtonElement,
+        state: false,
+      },
+      stop: {
+        button: this.container.querySelector('.stop-button') as HTMLButtonElement,
+        state: true,
+      },
+    };
   }
 
   render() {
-    const startEngineButton: HTMLButtonElement | null = this.container.querySelector('.start-button');
-    const stopEngineButton: HTMLButtonElement | null = this.container.querySelector('.stop-button');
-
-    startEngineButton?.addEventListener('click', () => {
-      // this.startEngine();
-
+    this.carControls.start.button.addEventListener('click', () => {
       this.startEngine();
     });
-    stopEngineButton?.addEventListener('click', () => {
+    this.carControls.stop.button.addEventListener('click', async () => {
       this.stopEngine();
     });
-
+    this.switchButtonsState();
     return this.container;
   }
 
+  startEngine = async () => {
+    if (this.engineStatus === ENGINESTATE.STARTED) {
+      console.log(`engine ${this.car.name} already started`);
+      return;
+    }
+
+    this.engineStatus = ENGINESTATE.STARTED;
+    const engine: IEngine = await this.loader.switchEngine(this.car.id, ENGINESTATE.STARTED);
+
+    animation(this.driver, engine.velocity, engine.distance, this.car.id);
+    this.switchButtonsState();
+    try {
+      await this.drive();
+    } catch (error) {
+      cancelAnimationFrame(Car.animations[this.car.id]);
+    }
+  };
+
   drive = async () => {
-    await this.loader.getData(`engine?id=${this.car.id}&status=drive`, METHODS.PATCH)
-      .then((res) => res.json())
-      .then((wrym) => console.log(wrym));
+    const engine = await this.loader.switchEngine(this.car.id, ENGINESTATE.DRIVE);
+    // .then((res) => console.log(res));
   };
 
   stopEngine = async () => {
-    await this.loader.getData(`engine?id=${this.car.id}&status=${ENGINESTATE.STOPPED}`, METHODS.PATCH)
-      .then((res: Response) => res.json())
+    await this.loader.switchEngine(this.car.id, ENGINESTATE.STOPPED)
       .then(() => {
-        this.velocity = null;
-        this.distance = null;
         this.engineStatus = ENGINESTATE.STOPPED;
+        this.switchButtonsState();
+        cancelAnimationFrame(Car.animations[this.car.id]);
+        this.driver.style.transform = `translateX(${0}px)`;
       });
-  };
-
-  startEngine = async () => {
-    if (this.engineStatus === ENGINESTATE.STARTED) {
-      console.log(`engine car ${this.car.name} already started`);
-      return;
-    }
-    await this.loader.getData(`engine?id=${this.car.id}&status=${ENGINESTATE.STARTED}`, METHODS.PATCH)
-      .then((res: Response) => res.json())
-      .then((engine: IEngine) => {
-        this.velocity = engine.velocity;
-        this.distance = engine.distance;
-        this.engineStatus = ENGINESTATE.STARTED;
-      })
-      .then(() => {
-        driveCar(this.carImage, this.velocity);
-      })
-      .then(() => this.drive());
   };
 
   private getCarHTML() {
@@ -80,30 +92,24 @@ export default class Car {
             <button data-id=${this.car.id} class="remove-button">remove</button>
             <button data-id=${this.car.id}  class="select">select</button>
             <span>${this.car.name}</span>
-            <div class="road">
+            <div class="garage__item">
               <button data-id=${this.car.id} id='start' class="start-button">a</button>
               <button data-id=${this.car.id} class="stop-button disabled-button">b</button>
-              <div class="car" id=${this.car.id}-img>
-               ${View.renderCarImage(this.car.color)}
-              </div>
-              <img class="flag" src=${flagImage} width="50">
+                <div class="car car_${this.car.id}">
+                ${View.renderCarImage(this.car.color)}
+                </div>
+              <div class="road road_${this.car.id}"></div>
+              <img class="flag finish_${this.car.id}" src=${flagImage} width="50">
+            </div>
           `;
+
     return carHTML;
   }
-}
 
-function driveCar(element: HTMLElement | null, velocity: number | null) {
-  const start = new Date().getTime();
-  const el = element!;
-
-  function step() {
-    const currentTime = new Date().getTime();
-    const progress = currentTime - start;
-
-    if (progress < 2000) {
-      requestAnimationFrame(step);
-    }
-  }
-
-  requestAnimationFrame(step);
+  switchButtonsState = () => {
+    Object.values(this.carControls).forEach((carController) => {
+      carController.button.disabled = carController.state;
+      carController.state = !carController.state;
+    });
+  };
 }
