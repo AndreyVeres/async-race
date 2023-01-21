06f889924cb868eb4c32268215aedb0b8f594be3
Andrey <spacecmetanka@gmail.com>
2023-01-21 20:14:49 +0300

    refactor: move const in other file

---
+++ async-race/src/types/consts.ts
@@ -0,0 +1,12 @@
+export enum METHODS {
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+  PATCH = 'PATCH'
+}
+
+export enum ENGINESTATE {
+  STARTED = 'started',
+  STOPPED = 'stopped',
+  DRIVE = 'drive'
+}
