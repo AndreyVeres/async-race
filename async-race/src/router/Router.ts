c8e32b770d4c8065d0d8b9de55a9913ba5038e81
Andrey <spacecmetanka@gmail.com>
2023-01-21 20:13:53 +0300

    feat: implement router

---
+++ async-race/src/router/Router.ts
@@ -0,0 +1,51 @@
+import { IRoute } from '../types/types';
+import MainPage from '../pages/MainPage';
+import Winners from '../pages/Winners';
+
+export default class Router {
+  private readonly routes: Array<IRoute>;
+  private defaultRoute: IRoute;
+
+  constructor(private rootElement: HTMLElement) {
+    this.routes = [
+      {
+        name: '/',
+        component: async () => {
+          this.rootElement.append(await new MainPage().render());
+        },
+      },
+      {
+        name: '/winners',
+        component: async () => {
+          this.rootElement.append(await new Winners().render());
+        },
+      },
+    ];
+
+    this.defaultRoute = {
+      name: 'Default router',
+      component: () => {
+        this.rootElement.innerHTML = '404 PAGE';
+      },
+    };
+  }
+
+  updateRouter(): void {
+    this.rootElement.innerHTML = '';
+    const currentRouteName = window.location.hash.slice(1);
+    const currentRoute = this.routes.find(
+      (page) => page.name === currentRouteName
+    );
+
+    (currentRoute || this.defaultRoute).component();
+  }
+
+  initRouter(): void {
+    if (window.location.hash === '') {
+      window.location.hash = '#/';
+    }
+
+    window.onpopstate = () => this.updateRouter();
+    this.updateRouter();
+  }
+}
